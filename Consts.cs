5f17e5c9ca62ccb043f88fcf2e7aeb6750e2bc10
Laura Blázquez <laura.blazquez.7e7@itb.cat>
2024-01-20 14:52:58 +0100

    Add files via upload

---
+++ Consts.cs
@@ -0,0 +1,12 @@
+﻿namespace Messages
+{
+    public class Consts
+    {
+        //Constantes menú inicio
+        public const string Welcome = "\n¡¡Bienvenido a AdventureQuest!!\n";
+        public const string Menu = "¿Qué desea hacer ahora mi señor?\n1. Iniciar una nueva aventura\n0. Salir";
+        public const string Bye = "\nGracias por jugar, espero pronto su regreso mi señor.";
+        public const string MsgErrorOption = "\nLa opción que ha introducido no aparece en el menú mi señor, deme otra opción:";
+        public const string MsgErrorMenu = "\nHa introducido 3 veces un valor no válido, reinicie el juego.";
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilsLibrary
+{
+    public class Utils
+    {
+        public static bool InRangValidation(int option, int begin, int end)
+        {
+            return option == begin || option == end;
+        }
+
+    }
+}
